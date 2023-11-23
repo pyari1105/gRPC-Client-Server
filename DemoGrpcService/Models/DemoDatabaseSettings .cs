e69a34dfefddf6db2fb8fff762cf915f1e34193e
Surat Pyari <surat.pyari@ajmerainfotech.com>
2023-11-23 16:17:22 +0530

    refactor

--- DemoGrpcService/Models/DemoDatabaseSettings .cs
+++ DemoGrpcService/Models/DemoDatabaseSettings .cs
@@ -3,14 +3,12 @@
     public class DemoDatabaseSettings : IDemoDatabaseSettings
     {
         public required string EmployeesCollectionName { get; set; }
-        public required string StudentsCollectionName { get; set; }
         public required string ConnectionString { get; set; }
         public required string DatabaseName { get; set; }
     }
     public interface IDemoDatabaseSettings
     {
         string EmployeesCollectionName { get; set; }
-        string StudentsCollectionName { get; set; }
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
     }
--- DemoGrpcService/Program.cs
+++ DemoGrpcService/Program.cs
@@ -7,12 +7,8 @@ using DemoGrpcService.Services;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddGrpc();
-
 builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
-
 var configs = KeyVaultConfigsService.GetConfigs(builder.Configuration);
     if (!string.IsNullOrWhiteSpace(configs.keyVaultUri))
     {
@@ -28,12 +24,6 @@ var configs = KeyVaultConfigsService.GetConfigs(builder.Configuration);
         builder.Services.AddControllers();
     }
 
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 app.MapGrpcService<EmployeeDetailService>();
-
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
